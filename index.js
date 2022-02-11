3c23f332fd7fc6424a7020e239b08e740d101a59
satyamsinha111 <satyam.sinha@navsoft.in>
2022-02-11 11:43:52 +0530

    added initial route

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Imports start here
 const express = require("express");
-const PORT = 3000 | process.env.PORT;
+const PORT = process.env.PORT || 3000;
 const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
